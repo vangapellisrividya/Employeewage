d7b9c6d1d38499a97e6bdf40597b472fd2e9b486
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 14:33:42 +0530

    [Srividya] Add: employee attendance

--- Employeewage.py
+++ Employeewage.py
@@ -24,10 +24,33 @@ class Employeewage:
         print(" Welcome to Employeewage Program on master")
         print("******************************************")
 
+    def attendance(self):
+        """
+            Description:
+                It wii employee is present or not in employeewage
+                program
+            Prameters:
+                self
+            Return:
+                None
+        """
+        self.ispresent  =   1
+        self.isabsent   =   0
+        attend = random.randint(0,1) 
+        while random.randint:
+            if attend == 1:
+                print(" Employee is Present ")
+            else:
+                print(" Employee is Absent")
+            break
+            
+
+
     
 if __name__=='__main__':
     emp=Employeewage()
     emp.display()
+    emp.attendance()
     
     
     
