7f8f335fff1a2bc8729bc340874397327713cdc6
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 14:26:32 +0530

    [Srividya] Add: display on master

---
+++ Employeewage.py
@@ -0,0 +1,33 @@
+''''
+    Created on Nov 24, 2021
+
+    @author:srividya
+    @Date:  24/11/2021
+    @Title: Employeewage
+
+    '''
+import random
+
+class Employeewage:
+
+    def display(self):
+        """
+            Description:
+                It wii display the welcome part in employeewage
+                program
+            Prameters:
+                self
+            Return:
+                None
+        """
+        print("*****************************************")
+        print(" Welcome to Employeewage Program on master")
+        print("******************************************")
+
+    
+if __name__=='__main__':
+    emp=Employeewage()
+    emp.display()
+    
+    
+    
