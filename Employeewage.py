c7acca4dfc9fd1ea06ba5b6eeea2f10f49d38548
vangapellisrividya <vangapellisreevidya@gmail.com>
2021-11-26 14:49:45 +0530

    [Srividya] Add: Employee parttime is added

--- Employeewage.py
+++ Employeewage.py
@@ -44,6 +44,29 @@ class Employeewage:
             else:
                 print(" Employee is Absent")
             break
+    
+    def parttime(self):
+        """
+            Description:
+                It wii decide employee present or not in employeewage
+                program
+            Prameters:
+                self
+            Return:
+                None
+        """
+        empCheck = random.randint(0,2) 
+        #print("Employee is present",empCheck)
+    
+        if empCheck == 1:
+            self.workinghours = 8
+            print(" Employee working fulltime ")
+        elif empCheck == 2 :
+            self.workinghours = 4
+            print(" Employee working parttime ")
+        else:
+            self.workinghours = 0
+            print(" Employee is absent ")
 
     def calculate_wage(self):
         """
@@ -73,6 +96,7 @@ if __name__=='__main__':
     emp=Employeewage()
     emp.display()
     emp.attendance()
+    emp.parttime()
     emp.calculate_wage()
     
     
